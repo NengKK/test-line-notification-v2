f96d8878bc9e59c45adae7c14f32f094ae022f3d
Kittisak Khamdaeng <kittisak.khamdaeng@gmail.com>
2021-07-28 12:15:52 +0700

    Handle location from query string, add more log

--- src/index.ts
+++ src/index.ts
@@ -11,29 +11,43 @@ app.get('/', (req, res) => {
 });
 
 app.get('/aqi', async (req, res) => {
+  let location: string;
+  if (req.query.location && req.query.location !== '') {
+    location = req.query.location?.toString();
+  } else {
+    location = process.env.DEFAULT_LOCATION_NAME ?? '';
+  }
+  
   const options: AxiosRequestConfig = {
     method: 'GET',
-    url: `${process.env.AQI_CN_URL}/search/?token=${process.env.AQI_CN_TOKEN}&keyword=${process.env.DEFAULT_LOCATION_NAME}`
+    url: `${process.env.AQI_CN_URL}/search/?token=${process.env.AQI_CN_TOKEN}&keyword=${encodeURI(location)}`
   };
-
   let message = 'DEFAULT';
   let aqiData: IAqiData;
+
+  console.log(`Location: ${location}`);
+  console.log(`URL: ${options.url}`);
 
   await axios(options)
     .then(async res => {
       if (res.data.status === 'ok') {
         if (res.data.data.length > 0) {
           let aqiJSONData = res.data.data[0];
+          console.log(`result: ${JSON.stringify(aqiJSONData)}`);
           aqiData = <IAqiData>aqiJSONData;
           message = `ปริมาณฝุ่น PM 2.5 ที่ ${aqiData.station.name} ณ วันที่ ${new Date(aqiData.time.stime).toLocaleString('th-TH')} คือ ${aqiData.aqi} (คุณภาพอากาศ${aqiClassification(+aqiData.aqi)})`;
         } else {
-          message = `ไม่พบข้อมูล AQI จากชื่อสถานที่ที่กำหนด. (${process.env.DEFAULT_LOCATION_NAME})`;
+          console.info(`Data not found: ${JSON.stringify(res.data)}`);
+          message = `ไม่พบข้อมูล AQI จากชื่อสถานที่ที่กำหนด. (${location})`;
         }
       } else {
         message = 'ไม่สามารถเรียกดูข้อมูล AQI จาก AQICN ได้';
       }
     })
-    .catch(() => message = 'เกิดข้อผิดพลาดในการเชื่อมต่อไปยัง AQICN');
+    .catch((error) => {
+      console.error(error);
+      message = 'เกิดข้อผิดพลาดในการเชื่อมต่อไปยัง AQICN';
+    });
 
   res.send(message);
 });
