7369d890f500aea954b127702eeb589742c54ab1
Kittisak Khamdaeng <kittisak.khamdaeng@gmail.com>
2021-08-13 23:38:03 +0700

    Remove testing code, add prettier config

--- src/routes/weather.ts
+++ src/routes/weather.ts
@@ -2,8 +2,5 @@ var router = require('express').Router();
 var handler = require('../lib/WeatherWarning/handler');
 
 router.get('/warning', handler.get);
-router.post('/post', (req: any, res: any) => {
-  console.log('ok');
-});
 
 module.exports = router;
